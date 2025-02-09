1cc29029e51e071bd36153ef0448e4667db06ce9
BulgakovDmitry <dima.olympiad11@yandex.ru>
2025-02-10 00:29:42 +0300

    delete copyPaste

--- second.asm
+++ second.asm
@@ -1,201 +1,143 @@
 .model tiny
 .code
 org 100h
-; di
+; глава 10 177
+
 Start:
-	mov ah, 09h                             ; printf ('Hello world')
-	mov dx, offset String		            ;
-	int 21h                                 ;
+	mov ah, 09h                           	     ; printf ("Hello world")
+	mov dx, offset String		                 ;
+	int 21h                                		 ;
 	                                  
-	mov bx, 0b800h		                    ; write to bx addr video mem
+	mov bx, 0b800h		                   		 ; write to bx addr video mem
 	
-	mov es, bx							    ;
-	mov bx, 700			                    ; start to position 500 in vid mem
+	mov es, bx							         ;
+	mov bx, 700			                         ; start to position 500 in vid mem
+	mov ah, 00110100b							 ; put to ah the color atribute 
+
+	mov dx, 7									 ; frame width
+	;mov sp, 48
+	call drawFrame								 ; draw frame
 	
-	call DrawtopBorder
-	call DrawString
-	call DrawString
-	call DrawString
-	call DrawString
-	call DrawString
-	call DrawString
-
-	call DrawWords
-
-	call DrawString
-	call DrawString
-	call DrawString
-	call DrawString
-	call DrawString
-	call DrawString
-	call DrawbottonBorder
-
-	mov ax, 4c00h							; return 0
-	int 21h									;
-
+	mov ax, 4c00h							     ; return 0
+	int 21h									     ;
 
 String: db "Hello World$"
 
 ;------------------------------------------------
-; Draw string in video mem
+; Draw frame in video mem
 ; Entry: None
 ; Exit : None
-; Destr: bx es cx
+; Destr: si, di
 
-DrawString  proc
-		    mov cx, 40 
+drawFrame	proc
 
-			mov si, offset ArraySymbols
-			mov dx, [si]
+			sub di, 1
 
-			call DrawSideBorder
+			mov si, offset ArraySymbols		     ; put string to si
+
+			call drawLine					     ; draw the top of border
+			add si, 3							 ; array pointer += 3
+
+			call drawSpace
+
+			call drawWords						 ; draw words in the middle of the frame 
+
+			mov si, offset ArraySymbols + 3
+
+			call drawSpace
+
+			add si, 3							 ; array pointer += 3
+			call drawLine						 ; draw the botton of border
+
+			ret
+endp
+;------------------------------------------------
+
+drawSpace 	proc
+
+			mov di, dx							 ; 
+			cycleForDrawingLine:				 ; draw space 
+				call drawLine				     ; 
+				sub di, 1					     ;
+				jnz cycleForDrawingLine 		 ;
+			ret
+endp
+
+;------------------------------------------------
+; Draw line of the frame (subfunction of drawFrame) 
+; Entry: None
+; Exit : None
+; Destr: cx, al
+
+drawLine    proc
+
+			mov al, [si]						 ; al = ArraySymbols[si]
+			call DrawChar						 ; 
 				
-			mov dx, [si+1]
+		    mov cx, 40                           ; number of repetitions
+			mov al, [si+1]						 ; al = ArraySymbols[si + 1]
 			CycleForString:
-				mov byte ptr es:[bx], dl
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
+				call DrawChar
 				loop CycleForString
 			
-			mov dx, [si+2]
-
-			call DrawSideBorder
+			mov al, [si+2]					     ; al = ArraySymbols[si + 2]
+			call DrawChar						 ; 
 
 			add bx, 76
 
 			ret 
-			endp
+endp
 
-DrawSideBorder  proc
+drawWords       proc
 
-				mov byte ptr es:[bx], dl
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
+				mov al, 186
+				call DrawChar
 
-				ret 
-				endp
-
-DrawtopBorder   proc
-
-				mov cx, 40
-
-				mov byte ptr es:[bx], 201
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
-
-				CycleForTopBorder:
-					mov byte ptr es:[bx], 205
-					add bx, 1                                     
-					mov byte ptr es:[bx], 00110100b
-					add bx, 1
-					loop CycleForTopBorder
-				
-				mov byte ptr es:[bx], 187
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
-
-				add bx, 76
-
-				ret 
-				endp
-
-DrawbottonBorder proc
-
-				mov cx, 40
-
-				mov byte ptr es:[bx], 200
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
-
-				CycleForBottonBorder:
-					mov byte ptr es:[bx], 205
-					add bx, 1                                     
-					mov byte ptr es:[bx], 00110100b
-					add bx, 1
-					loop CycleForBottonBorder
-				
-				mov byte ptr es:[bx], 188
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
-
-				add bx, 76
-
-				ret 
-				endp
-
-DrawWords       proc
-
-				mov byte ptr es:[bx], 186
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
-
-				mov cx, 17
-				CycleForWordsInFrame1:
-					mov byte ptr es:[bx], ' '
-					add bx, 1                                     
-					mov byte ptr es:[bx], 00110100b
-					add bx, 1
-					loop CycleForWordsInFrame1
+				mov di, 17
+				cycleForDrawingString1:				 ; draw space 
+					mov al, ' '
+					call DrawChar
+					sub di, 1
+					jnz cycleForDrawingString1 		 ;
 
 				mov si, offset WordsInFrame
 
-				mov dx, [si]
-				mov byte ptr es:[bx], dl
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
-				
-				mov dx, [si+1]
-				mov byte ptr es:[bx], dl
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
+				mov di, 5
+				cycleForDrawingString3:
+					mov al, [si]
+					call DrawChar
+					add si, 1
+					sub di, 1
+					jnz cycleForDrawingString3
 
-				mov dx, [si+2]
-				mov byte ptr es:[bx], dl
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
+				mov di, 18
+				cycleForDrawingString2:				 ; draw space 
+					mov al, ' '
+					call DrawChar
+					sub di, 1
+					jnz cycleForDrawingString2 		 ;
 
-				mov dx, [si+3]
-				mov byte ptr es:[bx], dl
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
-
-				mov dx, [si+4]
-				mov byte ptr es:[bx], dl
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
-
-				mov cx, 18
-				CycleForWordsInFrame2:
-					mov byte ptr es:[bx], ' '
-					add bx, 1                                     
-					mov byte ptr es:[bx], 00110100b
-					add bx, 1
-					loop CycleForWordsInFrame2
-
-				mov byte ptr es:[bx], 186
-				add bx, 1                                     
-				mov byte ptr es:[bx], 00110100b
-				add bx, 1
+				mov al, 186
+				call DrawChar
 
 				add bx, 76
 
 				ret 
 				endp
-;------------------------------------------------
 
-ArraySymbols: db 186, ' ', 186 
+DrawChar 		proc
+
+				mov byte ptr es:[bx], al
+				add bx, 1                                     
+				mov byte ptr es:[bx], ah
+				add bx, 1
+
+				ret 
+endp
+;------------------------------------------------ 
+
+ArraySymbols : db 201, 205, 187, 186, ' ', 186, 200, 205, 188 
+;ArraySymbols : db c9h, cdh, bbh, bah, ' ', bah, c8h, cdh, bch 
 WordsInFrame : db 'MEOW!' 
 
 end Start
